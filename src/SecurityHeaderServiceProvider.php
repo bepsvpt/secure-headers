5b68247a1a0e0b70a86fa67d57162faa471e4542
BePsvPT <freedom@csie.io>
2015-12-01 16:48:51 +0800

    Add service provider

---
+++ src/SecurityHeaderServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bepsvpt\LaravelSecurityHeader;
+
+use Illuminate\Support\ServiceProvider;
+
+class SecurityHeaderServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/security-header.php';
+
+        $publishPath = config_path('security-header.php');
+
+        $this->publishes([$configPath => $publishPath], 'config');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/security-header.php';
+
+        $this->mergeConfigFrom($configPath, 'security-header');
+    }
+}
