35536747b0a8ce496ed47749bd1b50e09b405260
BePsvPT <freedom@csie.io>
2016-08-24 10:50:44 +0800

    Fix missing helper function

--- tests/helpers.php
+++ tests/helpers.php
@@ -22,3 +22,18 @@ if (! function_exists('config')) {
         return require $path;
     }
 }
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable. Supports boolean, empty and null.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        return $default;
+    }
+}
