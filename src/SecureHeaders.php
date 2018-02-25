63dd69d621d878ea59ea4bd56910032e9b95211e
Enaah <mathis.bin@icloud.com>
2018-02-25 21:03:48 +0100

    fix/quotes
    - replaces double-quotes with single-quotes

--- src/SecureHeaders.php
+++ src/SecureHeaders.php
@@ -158,7 +158,7 @@ class SecureHeaders
             $hsts .= ' includeSubDomains;';
         }
 
-        $hsts .= " preload";
+        $hsts .= ' preload';
 
         return [
             'Strict-Transport-Security' => $hsts,
