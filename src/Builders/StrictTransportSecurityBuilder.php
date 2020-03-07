775c9cb471fde3565dcb2e039390fddbbc0070e9
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-03-07 09:44:39 +0800

    add strict-transport-security builder

---
+++ src/Builders/StrictTransportSecurityBuilder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bepsvpt\SecureHeaders\Builders;
+
+final class StrictTransportSecurityBuilder extends Builder
+{
+    /**
+     * @inheritDoc
+     */
+    public function get(): string
+    {
+        $directives[] = $this->maxAge();
+
+        if ($this->config['include-sub-domains'] ?? false) {
+            $directives[] = 'includeSubDomains';
+        }
+
+        if ($this->config['preload'] ?? false) {
+            $directives[] = 'preload';
+        }
+
+        return implode('; ', $directives);
+    }
+
+    /**
+     * Get max-age directive.
+     *
+     * @return string
+     */
+    protected function maxAge(): string
+    {
+        $origin = $this->config['max-age'] ?? 31536000;
+
+        // convert to int
+        $age = intval($origin);
+
+        // prevent negative value
+        $val = max($age, 0);
+
+        return sprintf('max-age=%d', $val);
+    }
+}
