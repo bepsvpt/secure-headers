a87c193a186252f0d22288fa20a76128e4e0fc5c
Jorge P. Hernandez Lalcebo <j.lalcebo@chargebacks911.com>
2020-11-04 13:53:50 -0500

    Fixed incorrect option header name.

--- src/SecureHeaders.php
+++ src/SecureHeaders.php
@@ -227,7 +227,7 @@ class SecureHeaders
             'X-Download-Options' => $this->config['x-download-options'],
             'X-Frame-Options' => $this->config['x-frame-options'],
             'X-Permitted-Cross-Domain-Policies' => $this->config['x-permitted-cross-domain-policies'],
-            'X-Powered-By' => $this->config['x-powered-by'] ?? ($this->config['x-power-by'] ?? ''),
+            'X-Powered-By' => $this->config['x-powered-by'] ?? ($this->config['x-powered-by'] ?? ''),
             'X-XSS-Protection' => $this->config['x-xss-protection'],
             'Referrer-Policy' => $this->config['referrer-policy'],
             'Server' => $this->config['server'],
